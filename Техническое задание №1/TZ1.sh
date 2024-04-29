8e860d9d749e2c11e5784605ff875bc92e6d0338
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-04-29 22:06:23 +0300

    Update TZ1.sh

--- Техническое задание №1/TZ1.sh
+++ Техническое задание №1/TZ1.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+echo "input_dir"
 read  input_dir # принять входную директорию
+echo "output_dir"
 read  output_dir # принять выходную директорию
-dir_list=($(find "$input_dir" -type d)) # список директориев, находящихся во входной директории
-level1_files_list=($(find "$input_dir" -maxdepth 1 -type f)) # список файлов, находящихся непосредственно во входной директории 
-all_file_list=($(find "$input_dir" -type f)) # список всех файлов, вложенных во входную директорию
+dir_list=()
+level1_files_list=()
+all_file_list=()
+for i in "$input_dir"/*; do
+	if [[ -d "$i" && -x "$i" && ! "$(basename "$i")" = ".*" ]]; then
+		dir_list+=("$i")
+	fi
+	if [[ -f "$i" && $(basename "$i" | grep -c '/') -eq 0 && ! -h "$i" && -x "$i" && "$(basename "$i")" != .* ]]; then
+		level1_files_list+=("$i")
+	elif [[ -f "$i" && ! -h "$i" && -x "$i" && ! "$(basename "$i")" = ".*" ]]; then
+		all_file_list+=("$i")
+	fi
+done
 for i in "${all_file_list[@]}"; do # перебираем все файлы входной директории
     name_of_file=$(basename "$i")
-    if [[ -e "$output_dir/$name_of_file" ]]; then # если уже существует файл с таким же названием в выходной директории
+    if [ -e "$output_dir/$name_of_file" ]; then # если уже существует файл с таким же названием в выходной директории
         change_for_same_files=1 
-        while [[ -e "$output_dir/$name_of_file.$change_for_same_files" ]]; do # добавляем номер к названию файла, пока не найдем отличающееся имя
+        while [ -e "$output_dir/$name_of_file.$change_for_same_files" ]; do # добавляем номер к названию файла, пока не найдем отличающееся имя
              change_for_same_files=$((change_for_same_files + 1))
         done 
         cp -p "$i" "$output_dir/$name_of_file.$change_for_same_files" #копируем файл с уникальным именем
@@ -16,3 +28,4 @@ for i in "${all_file_list[@]}"; do # перебираем все файлы вх
         cp -p "$i" "$output_dir" #копируем файл в выходную директорию
     fi
 done
+
