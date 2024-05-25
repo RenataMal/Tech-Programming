150fc6189f75bc753e3ae9f844287bbd2114cdf1
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-05-25 21:08:12 +0300

    Add files via upload

--- TZ2/src/test/java/MainTest.java
+++ TZ2/src/test/java/MainTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Assertions;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -10,38 +8,38 @@ class MainTest {
 
     @Test
     void min() {
-        long[] numbers = {3, 46436, 546, 23, 6};
+        int[] numbers = {3, 46436, 546, 23, 6};
 
-        long answer = Main.min(numbers);
+        int answer = Main.min(numbers);
 
-        assertEquals(3, answer);
+        assertEquals(3, answer, "Не работает.");
     }
 
     @Test
     void max() {
-        long[] numbers = {43, 5, 564, 3454, 6454, 322, 536};
+        int[] numbers = {43, 5, 564, 3454, 6454, 322, 536};
 
-        long answer = Main.max(numbers);
+        int answer = Main.max(numbers);
 
-        assertEquals(6454, answer);
+        assertEquals(6454, answer, "Не работает.");
     }
 
     @Test
     void sum() {
-        long[] numbers = {1, 1, 454, 656, 34, 456, 889};
+        int[] numbers = {1, 1, 454, 656, 34, 456, 889};
 
-        long answer = Main.sum(numbers);
+        int answer = Main.sum(numbers);
 
-        assertEquals(2491, answer);
+        assertEquals(2491, answer, "Не работает.");
     }
 
     @Test
     void mult() {
-        long[] numbers = {5, 3, 5, 7, 10, 657};
+        int[] numbers = {5, 3, 5, 7, 10, 657};
 
         long answer = Main.mult(numbers);
 
-        assertEquals(3449250, answer);
+        assertEquals(3449250, answer, "Не работает.");
     }
 
     @Test
@@ -49,8 +47,8 @@ class MainTest {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
-            long[] numbers = new long[size];
-            Arrays.setAll(numbers, i -> (long) r.nextInt(100));
+            int[] numbers = new int[size];
+            Arrays.setAll(numbers, i -> r.nextInt(100));
             long start = System.nanoTime();
             Main.min(numbers);
             long end = System.nanoTime();
@@ -64,8 +62,8 @@ class MainTest {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
-            long[] numbers = new long[size];
-            Arrays.setAll(numbers, i -> (long) r.nextInt(100));
+            int[] numbers = new int[size];
+            Arrays.setAll(numbers, i -> r.nextInt(100));
             long start = System.nanoTime();
             Main.max(numbers);
             long end = System.nanoTime();
@@ -79,8 +77,8 @@ class MainTest {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
-            long[] numbers = new long[size];
-            Arrays.setAll(numbers, i -> (long) r.nextInt(100));
+            int[] numbers = new int[size];
+            Arrays.setAll(numbers, i -> r.nextInt(100));
             long start = System.nanoTime();
             Main.sum(numbers);
             long end = System.nanoTime();
@@ -94,24 +92,14 @@ class MainTest {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
-            long[] numbers = new long[size];
-            Arrays.setAll(numbers, i ->(long) r.nextInt(100));
+            int[] numbers = new int[size];
+            Arrays.setAll(numbers, i -> r.nextInt(100));
             long start = System.nanoTime();
             Main.mult(numbers);
             long end = System.nanoTime();
             long all_time = end - start;
             System.out.println("Количество чисел в файле: " + size + ". Время выполнения нахождения произведения всех чисел в наносекундах: " + all_time);
         }
-    }
-
-    @Test
-    void extra_test_timeout_min() {
-        long[] numbers = {323, 23, 4, 435, 435, 43663, 21134, 78970, 13654768, 42545};
-
-        Assertions.assertTimeout(Duration.ofNanos(1401), () -> {
-            Thread.sleep(1);
-            Main.min(numbers);
-        });
     }
 
 }
