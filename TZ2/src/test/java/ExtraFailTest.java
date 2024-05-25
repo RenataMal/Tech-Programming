d99fa8368969a0b93d5e6d0b362c8e9cd33e0e53
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-05-25 21:09:49 +0300

    Add files via upload

---
+++ TZ2/src/test/java/ExtraFailTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.time.Duration;
+
+public class ExtraFailTest {
+    @Test
+    void extra_test_timeout_min() {
+        int[] numbers = {323, 23, 4, 435, 435, 43663, 21134, 78970, 13654768, 42545};
+
+        Assertions.assertTimeout(Duration.ofNanos(140), () -> {
+            Main.min(numbers);
+        });
+    }
+}
