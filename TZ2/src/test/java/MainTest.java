5c1845558f6def5018b66c3f5ff8a2c8eba3b403
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-05-25 16:54:55 +0300

    Update MainTest.java

--- TZ2/src/test/java/MainTest.java
+++ TZ2/src/test/java/MainTest.java
@@ -9,50 +9,50 @@ import java.util.Random;
 class MainTest {
 
     @Test
-    void _min() {
+    void min() {
         long[] numbers = {3, 46436, 546, 23, 6};
 
-        long answer = Main._min(numbers);
+        long answer = Main.min(numbers);
 
         assertEquals(3, answer);
     }
 
     @Test
-    void _max() {
+    void max() {
         long[] numbers = {43, 5, 564, 3454, 6454, 322, 536};
 
-        long answer = Main._max(numbers);
+        long answer = Main.max(numbers);
 
         assertEquals(6454, answer);
     }
 
     @Test
-    void _sum() {
+    void sum() {
         long[] numbers = {1, 1, 454, 656, 34, 456, 889};
 
-        long answer = Main._sum(numbers);
+        long answer = Main.sum(numbers);
 
         assertEquals(2491, answer);
     }
 
     @Test
-    void _mult() {
+    void mult() {
         long[] numbers = {5, 3, 5, 7, 10, 657};
 
-        long answer = Main._mult(numbers);
+        long answer = Main.mult(numbers);
 
         assertEquals(3449250, answer);
     }
 
     @Test
-    void _min_speed() {
+    void min_speed() {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
             long[] numbers = new long[size];
             Arrays.setAll(numbers, i -> (long) r.nextInt(100));
             long start = System.nanoTime();
-            Main._min(numbers);
+            Main.min(numbers);
             long end = System.nanoTime();
             long all_time = end - start;
             System.out.println("Количество чисел в файле: " + size + ". Время выполнения поиска минимума в наносекундах: " + all_time);
@@ -60,14 +60,14 @@ class MainTest {
     }
 
     @Test
-    void _max_speed() {
+    void max_speed() {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
             long[] numbers = new long[size];
             Arrays.setAll(numbers, i -> (long) r.nextInt(100));
             long start = System.nanoTime();
-            Main._max(numbers);
+            Main.max(numbers);
             long end = System.nanoTime();
             long all_time = end - start;
             System.out.println("Количество чисел в файле: " + size + ". Время выполнения поиска максимума в наносекундах: " + all_time);
@@ -75,14 +75,14 @@ class MainTest {
     }
 
     @Test
-    void _sum_speed() {
+    void sum_speed() {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
             long[] numbers = new long[size];
             Arrays.setAll(numbers, i -> (long) r.nextInt(100));
             long start = System.nanoTime();
-            Main._sum(numbers);
+            Main.sum(numbers);
             long end = System.nanoTime();
             long all_time = end - start;
             System.out.println("Количество чисел в файле: " + size + ". Время выполнения вычисления суммы всех чисел в наносекундах: " + all_time);
@@ -90,14 +90,14 @@ class MainTest {
     }
 
     @Test
-    void _mult_speed() {
+    void mult_speed() {
         int[] size_of_array = {10, 100, 1000, 10000, 100000, 1000000};
         for (int size : size_of_array) {
             Random r = new Random();
             long[] numbers = new long[size];
             Arrays.setAll(numbers, i ->(long) r.nextInt(100));
             long start = System.nanoTime();
-            Main._mult(numbers);
+            Main.mult(numbers);
             long end = System.nanoTime();
             long all_time = end - start;
             System.out.println("Количество чисел в файле: " + size + ". Время выполнения нахождения произведения всех чисел в наносекундах: " + all_time);
@@ -110,7 +110,7 @@ class MainTest {
 
         Assertions.assertTimeout(Duration.ofNanos(1401), () -> {
             Thread.sleep(1);
-            Main._min(numbers);
+            Main.min(numbers);
         });
     }
 
