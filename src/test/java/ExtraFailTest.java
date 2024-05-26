19e7a2da6a3e5a702325ab994404d12b9f122ef3
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-05-26 17:22:32 +0300

    Update ExtraFailTest.java

--- src/test/java/ExtraFailTest.java
+++ src/test/java/ExtraFailTest.java
@@ -8,7 +8,7 @@ public class ExtraFailTest {
     void extra_test_timeout_min() {
         int[] numbers = {323, 23, 4, 435, 435, 43663, 21134, 78970, 13654768, 42545};
 
-        Assertions.assertTimeout(Duration.ofMillis(1), () -> {
+        Assertions.assertTimeout(Duration.ofNanos(1), () -> {
             Main.min(numbers);
         });
     }
