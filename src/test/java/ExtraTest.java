1d5ffeab36ffa905a1d543711b6a061475bf28d6
MalSoul <48159129+RenataMal@users.noreply.github.com>
2024-05-26 19:11:10 +0300

    Update ExtraTest.java

--- src/test/java/ExtraTest.java
+++ src/test/java/ExtraTest.java
@@ -6,10 +6,12 @@ import java.time.Duration;
 public class ExtraTest {
     @Test
     void extra_test_timeout_min() {
+        // Создаем массив чисел для тестирования
         int[] numbers = {323, 23, 4, 435, 435, 43663, 21134, 78970, 13654768, 42545};
 
+        // Проверяем, что метод Main.min() выполняется за указанное время
         Assertions.assertTimeoutPreemptively(Duration.ofMillis(500), () -> {
-            Main.min(numbers);
+            Main.min(numbers); // Вызываем метод для нахождения минимального числа
         });
     }
 }
